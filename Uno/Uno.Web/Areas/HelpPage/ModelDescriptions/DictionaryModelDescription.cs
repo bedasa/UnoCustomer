00e88e66e8af7bfe0b91e189a5964143b32bd655
bedasa <v-bedasa@hotmail.com>
2015-10-13 21:20:38 +0530

    deleted solution

--- Uno/Uno.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Uno.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Uno/Uno.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Uno.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
