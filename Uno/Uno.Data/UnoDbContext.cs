3ac3247be6964a3d9edcb563d2c1e5fc7bac15f2
bedasa <v-bedasa@hotmail.com>
2015-10-28 07:02:09 +0530

    code added

---
+++ Uno/Uno.Data/UnoDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uno.Data.Entities;
+
+namespace Uno.Data
+{
+    public class UnoDbContext : DbContext
+    {
+        public UnoDbContext() : this("DevUnoConnection")
+        {
+        }
+
+        public UnoDbContext(string connectionStringName) : base(connectionStringName)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Uno/Uno.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Uno.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
